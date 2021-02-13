8f36238b4ee5650da9ed5d99c445167d0a3489ea
Yaroslav-Naft <71295049+Yaroslav-Naft@users.noreply.github.com>
2021-02-12 23:50:04 -0800

    macincloud commit

---
+++ app/src/main/java/com/yaroslavnaft/roominclass/AddValueActivity.kt
@@ -0,0 +1,5 @@
+package com.yaroslavnaft.roominclass
+
+class AddValueActivity {
+
+}
--- app/src/main/java/com/yaroslavnaft/roominclass/MainActivity.kt
+++ app/src/main/java/com/yaroslavnaft/roominclass/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.yaroslavnaft.roominclass
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val fab = findViewById<FloatingActionButton>(R.id.fab)
+//        fab.setOnClickListener{
+//            val intent = Intent(this@MainActicity, AddValueActivity::class.java)
+//            startActivityForResult(intent, 1)
+//        }
     }
+
+
+
+
+
+
 }
